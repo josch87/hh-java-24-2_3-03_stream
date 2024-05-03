8f6f3658216038670d26de0911e02e9b01ea098a
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-05-03 11:11:10 +0200

    Add Main class with numbers list creation
    A new Main class has been added. This class contains a main method that creates a list of numbers from 0 to 19 and prints this list.

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        int lengthOfList = 20;
+
+        for (int i = 0; i < lengthOfList; i++) {
+            numbers.add(i);
+        }
+
+        System.out.println(numbers);
+
+
+    }
+}
