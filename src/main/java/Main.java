3faf4b88ddcb7fa1eaea72484be34ecdaaa393c6
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-05-03 11:21:35 +0200

    Add stream filtering for even numbers in Main.java
    A new stream filter was added to the main method in the Main.java class. This filter is used to extract even numbers from the 'numbers' list and print them onto the console. The filtered list of even numbers is also printed onto the console for verification.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,6 +12,9 @@ public class Main {
         }
 
         System.out.println(numbers);
+
+        List<Integer> evenNumbers = numbers.stream().filter((number) -> {return number % 2 == 0;}).peek(System.out::println).collect(Collectors.toList());
+        System.out.println(evenNumbers);
 
 
     }
